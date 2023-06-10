e00153e6e606a685597ecb382589e48a1933d75a
Kashif Khan <88695658+KashifKhaan@users.noreply.github.com>
2023-06-10 17:12:52 +0500

    Update and rename SERVER.java to ServerGUI.java

---
+++ ServerGUI.java
@@ -0,0 +1,134 @@
+package Chat_Application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.*;
+public class ServerGUI extends JFrame {
+
+    private JTextPane chatArea;
+    private JTextField inputField;
+    private JButton sendButton;
+    private ServerSocket server;
+    private Socket socket;
+    private BufferedReader br;
+    private PrintWriter out;
+
+    public ServerGUI() {
+        setTitle("Chat Server");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+
+        chatArea = new JTextPane();
+        chatArea.setEditable(false);
+        chatArea.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 14)); // Adjust the font size as needed
+
+        // Create a custom document to apply different colors to messages
+        StyledDocument styledDocument = chatArea.getStyledDocument();
+        Style regularStyle = StyleContext.getDefaultStyleContext().getStyle(StyleContext.DEFAULT_STYLE);
+        Style incomingMessageStyle = styledDocument.addStyle("incoming", regularStyle);
+        StyleConstants.setForeground(incomingMessageStyle, Color.BLUE);
+        Style outgoingMessageStyle = styledDocument.addStyle("outgoing", regularStyle);
+        StyleConstants.setForeground(outgoingMessageStyle, Color.RED);
+
+        JScrollPane scrollPane = new JScrollPane(chatArea);
+        scrollPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+        add(scrollPane, BorderLayout.CENTER);
+
+        JPanel inputPanel = new JPanel(new BorderLayout());
+        inputField = new JTextField();
+        sendButton = new JButton("Send");
+        sendButton.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14)); // Adjust the font size and style as needed
+        inputPanel.add(inputField, BorderLayout.CENTER);
+        inputPanel.add(sendButton, BorderLayout.EAST);
+        inputPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        add(inputPanel, BorderLayout.SOUTH);
+
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sendMessage();
+            }
+        });
+
+        setVisible(true);
+
+        try {
+            server = new ServerSocket(7778);
+            JOptionPane.showMessageDialog(this, "Server is ready for client requests. Run your client.", "Server Ready", JOptionPane.INFORMATION_MESSAGE);
+            socket = server.accept();
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+            startReading();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startReading() {
+        // thread
+        Runnable r1 = () -> {
+            appendToChatArea("READER STARTED....", "regular");
+
+            try {
+                while (true) {
+                    String msg = br.readLine();
+                    if (msg.equals("exit")) {
+                        appendToChatArea("SERVER TERMINATED", "regular");
+                        socket.close();
+                        break;
+                    }
+                    appendToChatArea("Client: " + msg, "incoming");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(r1).start();
+    }
+
+    private void sendMessage() {
+        String content = inputField.getText();
+        out.println(content);
+        out.flush();
+        inputField.setText("");
+
+        if (content.equals("exit")) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            appendToChatArea("Server: " + content, "outgoing");
+        }
+    }
+
+    private void appendToChatArea(String message, String style) {
+        StyledDocument styledDocument = chatArea.getStyledDocument();
+        try {
+            styledDocument.insertString(styledDocument.getLength(), message + "\n", styledDocument.getStyle(style));
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ServerGUI();
+            }
+        });
+    }
+}
+
+
